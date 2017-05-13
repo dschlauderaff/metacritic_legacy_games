9c673ef35a83a7e9c01457981ec03999fb4d558a
Dave Schlauderaff <dschlaud@gmail.com>
2017-05-13 09:40:46 -0500

    gem created using bundle gem

---
+++ lib/metacritic_legacy_games.rb
@@ -0,0 +1,5 @@
+require "metacritic_legacy_games/version"
+
+module MetacriticLegacyGames
+  # Your code goes here...
+end
---
+++ spec/metacritic_legacy_games_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe MetacriticLegacyGames do
+  it "has a version number" do
+    expect(MetacriticLegacyGames::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
